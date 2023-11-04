761795afe208d2a271994023e28482a5ed779cd9
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2023-11-04 14:53:08 +0100

    Added `Array_insert`, `Array_slice` and `Array_splice` methods + tests

--- test/internal/Array.test.c
+++ test/internal/Array.test.c
@@ -105,7 +105,6 @@ DESCRIBE(set, "Array_set") {
 
 		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), 10);
 	})
-
 }
 
 
@@ -263,6 +262,95 @@ DESCRIBE(pop, "Array_pop") {
 	})
 }
 
+DESCRIBE(insert, "Array_insert") {
+	Array *arr = NULL;
+
+	TEST("Inserts a value into empty array", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+
+		arr = Array_alloc(10);
+		Array_insert(arr, 0, &num1);
+
+		EXPECT_EQUAL_INT(arr->size, 1);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num1);
+
+		Array_insert(arr, 0, &num2);
+
+		EXPECT_EQUAL_INT(arr->size, 2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 1)), num1);
+
+		Array_insert(arr, 1, &num3);
+
+		EXPECT_EQUAL_INT(arr->size, 3);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 1)), num3);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 2)), num1);
+	})
+
+	TEST("Inserts a value into non-empty array", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+
+		Array_insert(arr, 1, &num4);
+
+		EXPECT_EQUAL_INT(arr->size, 4);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num1);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 1)), num4);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 2)), num2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 3)), num3);
+	})
+
+	TEST("Inserts a value past the end", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+
+		Array_insert(arr, 3, &num4);
+
+		EXPECT_EQUAL_INT(arr->size, 4);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num1);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 1)), num2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 2)), num3);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 3)), num4);
+	})
+
+	TEST("Inserts a value at negative index", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+
+		Array_insert(arr, -1, &num4);
+
+		EXPECT_EQUAL_INT(arr->size, 4);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 0)), num1);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 1)), num2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 2)), num4);
+		EXPECT_EQUAL_INT(*((int*)Array_get(arr, 3)), num3);
+	})
+}
+
 DESCRIBE(remove, "Array_remove") {
 	Array *arr = NULL;
 
@@ -358,6 +446,140 @@ DESCRIBE(clear, "Array_clear") {
 		Array_clear(arr);
 		EXPECT_EQUAL_INT(arr->size, 0);
 		EXPECT_EQUAL_PTR(Array_get(arr, 0), NULL);
+	})
+}
+
+DESCRIBE(arr_slice, "Array_slice") {
+	Array *arr = NULL;
+
+	TEST("Slices empty array", {
+		arr = Array_alloc(10);
+
+		Array *slice = Array_slice(arr, 1, 3);
+		EXPECT_EQUAL_INT(slice->size, 0);
+		EXPECT_EQUAL_PTR(Array_get(slice, 0), NULL);
+	})
+
+	TEST("Slices non-empty array", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *slice = Array_slice(arr, 1, 3);
+		EXPECT_EQUAL_INT(slice->size, 2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 1)), 30);
+	})
+
+	TEST("Slices array with negative indices", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *slice = Array_slice(arr, -3, -1);
+		EXPECT_EQUAL_INT(slice->size, 2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 1)), 30);
+	})
+
+	TEST("Slices array with out-of-bounds indices", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *slice = Array_slice(arr, -3, 10);
+		EXPECT_EQUAL_INT(slice->size, 3);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 1)), 30);
+		EXPECT_EQUAL_INT(*((int*)Array_get(slice, 2)), 40);
+	})
+}
+
+DESCRIBE(arr_splice, "Array_splice") {
+	Array *arr = NULL;
+
+	TEST("Splices empty array", {
+		arr = Array_alloc(10);
+
+		Array *splice = Array_splice(arr, 1, 3);
+		EXPECT_EQUAL_INT(splice->size, 0);
+		EXPECT_EQUAL_PTR(Array_get(splice, 0), NULL);
+	})
+
+	TEST("Splices non-empty array", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *splice = Array_splice(arr, 1, 3);
+		EXPECT_EQUAL_INT(splice->size, 2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 1)), 30);
+	})
+
+	TEST("Splices array with negative indices", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *splice = Array_splice(arr, -3, -1);
+		EXPECT_EQUAL_INT(splice->size, 2);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 1)), 30);
+	})
+
+	TEST("Splices array with out-of-bounds indices", {
+		int num1 = 10;
+		int num2 = 20;
+		int num3 = 30;
+		int num4 = 40;
+
+		arr = Array_alloc(10);
+		Array_push(arr, &num1);
+		Array_push(arr, &num2);
+		Array_push(arr, &num3);
+		Array_push(arr, &num4);
+
+		Array *splice = Array_splice(arr, -3, 10);
+		EXPECT_EQUAL_INT(splice->size, 3);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 0)), 20);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 1)), 30);
+		EXPECT_EQUAL_INT(*((int*)Array_get(splice, 2)), 40);
 	})
 }
 
