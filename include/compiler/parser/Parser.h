6ad908ee11381d5bb108ea941601dd0ed2575263
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2023-12-02 23:08:08 +0100

    Added author information

--- include/compiler/parser/Parser.h
+++ include/compiler/parser/Parser.h
@@ -1,6 +1,6 @@
 /**
  * @file include/compiler/parser/Parser.h
- * @author Author Name <xlogin00@stud.fit.vutbr.cz>
+ * @author Jaroslav Louma <xlouma00@stud.fit.vutbr.cz>
  * @brief This file is part of the IFJ23 project.
  * @copyright Copyright (c) 2023
  */
