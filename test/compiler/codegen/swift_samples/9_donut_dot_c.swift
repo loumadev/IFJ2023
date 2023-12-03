f768fdbc807b8447aa912335b33493e050aa70a3
Jaroslav Louma <jaroslav.louma.ml@gmail.com>
2023-12-03 18:18:32 +0100

    Optimize donut.c sample code

--- test/compiler/codegen/swift_samples/9_donut_dot_c.swift
+++ test/compiler/codegen/swift_samples/9_donut_dot_c.swift
@@ -31,8 +31,9 @@
 
 let PI = 3.141592
 let HPI = PI * 0.5
+let TPI = PI * 2.0
 
-func sin(_ x: Double) -> Double {
+func _sin(_ x: Double) -> Double {
     var result = 0.0
     var sign = 1.0
     var power = 1.0
@@ -44,8 +45,8 @@ func sin(_ x: Double) -> Double {
     return result
 }
 
-func cos(_ x: Double) -> Double {
-    return sin(HPI - x)
+func _cos(_ x: Double) -> Double {
+    return _sin(HPI - x)
 }
 
 func pow(_ x: Double, _ y: Double) -> Double {
@@ -68,34 +69,55 @@ func factorial(_ n: Double) -> Double {
     return result
 }
 
+var s = TPI / 254
+var i = 0.0
+var hS = ""
+while(i < TPI) {
+    hS = hS + chr(Double2Int((_cos(i) + 1) * 127))
+    i = i + s
+}
+
+func cos(_ x: Double) -> Double {
+	var _x = x
+	while(_x < 0) {
+		_x = _x + TPI
+	}
+	while(_x > TPI) {
+		_x = _x - TPI
+	}
+	var i = Double2Int(_x / TPI * 255)
+	return Int2Double(ord(substring(of: hS, startingAt: i, endingBefore: i + 1)!)) / 127 - 1
+}
+
+func sin(_ x: Double) -> Double {
+	return cos(x + HPI)
+}
+
 
 var A = 1.0,
 	B = 1.0
+var R = "                                                                               ",
+	Rb = R + "\n",
+	Rz = R + " "
+var hB = "", hZ = ""
+var k = 0
+while(k < 22) {
+	hB = hB + Rb
+	hZ = hZ + Rz
+	k = k + 1
+}
+
 while(true) {
-	var b = ""
-	var z = ""
-	A = A + 0.07
-	B = B + 0.03
+	var b = hB
+	var z = hZ
+	A = A + 0.15
+	B = B + 0.075
 	var cA = cos(A),
 		sA = sin(A),
 		cB = cos(B),
 		sB = sin(B)
 	var m = 0
 	var k = 0
-	var l = 1760
-	while(k < l) {
-		if(m == 79) {
-			b = b + "\n"
-			m = 0
-		}
-		else {
-			b = b + " "
-			m = m + 1
-		}
-		z = z + " "
-
-		k = k + 1
-	}
 	var j = 0.0
 	while(j < 6.28) {
 		// j <=> theta
@@ -120,16 +142,16 @@ while(true) {
 							st * cA -
 							cp * ct * sB))
 
-			if(o >= 0 && o < l) {
+			if(o >= 0 && o < 1760) {
 				var z_o = Int2Double(ord(substring(of: z, startingAt: o, endingBefore: o + 1)!)) / 255.0
 				if(y < 22 && y >= 0 && x >= 0 && x < 79 && D > z_o) {
-					z = substring(of: z, startingAt: 0, endingBefore: o)! + chr(Double2Int(D * 255)) + substring(of: z, startingAt: o + 1, endingBefore: l)!
+					z = substring(of: z, startingAt: 0, endingBefore: o)! + chr(Double2Int(D * 255)) + substring(of: z, startingAt: o + 1, endingBefore: 1760)!
 					var n = 0
 					if(N > 0) {
 						n = N
 					}
 					var c = substring(of: ".,-~:;=!*#$@", startingAt: n, endingBefore: n + 1)!
-					b = substring(of: b, startingAt: 0, endingBefore: o)! + c + substring(of: b, startingAt: o + 1, endingBefore: l)!
+					b = substring(of: b, startingAt: 0, endingBefore: o)! + c + substring(of: b, startingAt: o + 1, endingBefore: 1760)!
 				}
 			}
 
